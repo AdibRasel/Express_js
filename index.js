9db734177ac4dc0e11c85a52a524b605872486c4
AdibRasel <97701792+AdibRasel@users.noreply.github.com>
2022-07-29 00:33:23 +0600

    Middleware
    Middleware

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
+
+// const express = require("express");
+// const multer = require("multer")
+// app = express()
+
+// var storage = multer.diskStorage({
+//    destination:function (req, file, callBack) {
+//       callBack(null, "uploads")
+//    },
+//    filename:function(req, file, callback){
+//       callback(null, file.originalname);
+//    }
+// })
+// var upload = multer({storage : storage}).single("myfile")
+
+// app.post('/' , (req , res)=>{
+//    upload(req, res, function(error){
+//       if(error){
+//          res.send("File Uploade Failde")
+//          res.end()
+//       }else{
+//          res.send("File Uploade Success")
+//          res.end()
+//       }
+//    })
+// })
+// app.listen(4000, ()=>{
+//    console.log("Your Server is Runing")
+// })
+
+
 const express = require("express");
-const body_parser = require("body-parser")
 app = express();
 
-app.use(body_parser.json())
-
-app.post('/' , (req , res)=>{
-
-   const jsonData = req.body;
-   const jsonString = JSON.stringify(jsonData)
-
-   const DataJson = jsonData.name;
-   const student_sub = jsonData.sub[2]
-
-   res.end(jsonString + DataJson + student_sub)
-
-
+app.use( "/about" , function(req, res, next){
+   console.log("শুধু মাত্র /about রাউট এর জন্য কাজ করবে।");
+   next();
 })
-app.listen(2000, ()=>{
-   console.log("your Server is Runing Success")
+
+app.get('/' , (req , res)=>{
+   res.send('')
 })
+app.get('/contact' , (req , res)=>{
+   res.send('')
+})
+app.get('/about' , (req , res)=>{
+   res.send('Fine')
+})
+
+app.listen(3000, ()=>{
+   console.log("Your Server Running Success")
+})
+
+
 
